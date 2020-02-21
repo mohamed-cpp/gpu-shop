cfbba8ae00bf821d3ed6a07a658f843c8c5fb809
vagrant <vagrant@homestead>
2020-02-21 22:10:24 +0000

    fix admin

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,10 +31,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        if (auth('web')->check()){
-            Gate::before(function ($user, $ability) {
-                return $user->hasRole('Super Admin') ? true : null;
-            });
-        }
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('Super Admin') ? true : null;
+        });
+
     }
 }
